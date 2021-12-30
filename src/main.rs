9131381b1c76e41d923d8ad32e89545989420256
mohamedh2000 <mohamed.h@northeastern.edu>
2021-12-29 23:50:07 -0500

    Finished adding USD balances

--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,59 @@ use::crypto::mac::Mac;
 use::base64::{ encode };
 use::reqwest;
 use::serde_json::Value;
-use std::collections::HashMap;
+use std::{collections::HashMap};
+
+#[derive()]
+struct KucoinCred<'a> { 
+    kucoin_api_key : &'a str,
+    kucoin_passphrase : &'a str,
+    kucoin_secret : &'a str,
+    kucoin_base_uri : &'a str,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    const KUCOIN_API_KEY : &str = "61c83393d98e2200016519ea";
-    const KUCOIN_PASSPHRASE : &str = "M?m5cBP?ko4fJG9A";
-    const KUCOIN_SECRET : &str = "b96b57e7-8534-4aa0-9a44-61e1666c5e67";
-    const KUCOIN_BASE_URI : &str = "https://api.kucoin.com";
-    const KUCOIN_ENDPOINT : &str = "/api/v1/accounts";
 
-    //constructing the timestamp 
-    let time_stamp : u128 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+    let kucoin_acc_endpoint : &str =  "/api/v1/accounts";
+    let kucoin_price_endpoint : &str = "/api/v1/market/orderbook/level1";
 
-    let sign_str : &str = &format!("{}{}{}{}", &time_stamp, "GET", &KUCOIN_ENDPOINT, "");
-    let kc_sign : String = sign(sign_str, &KUCOIN_SECRET);
+    let current_user :KucoinCred = KucoinCred {
+        kucoin_base_uri : "https://api.kucoin.com",
+        kucoin_secret : "b96b57e7-8534-4aa0-9a44-61e1666c5e67",
+        kucoin_api_key : "61c83393d98e2200016519ea",
+        kucoin_passphrase : "M?m5cBP?ko4fJG9A", 
+    };
+    
+    let res_str: String = get_kucoin_response(&kucoin_acc_endpoint, &current_user).await?;
 
-    let kc_pass_sign : String = sign(KUCOIN_PASSPHRASE, &KUCOIN_SECRET);
-    let kucoin_version : &str = "2";
-
-    let uri = format!("{}{}",KUCOIN_BASE_URI, KUCOIN_ENDPOINT);
-
-    let client = reqwest::Client::new();
-
-    let res = client.get(uri)
-            .header("KC-API-KEY", KUCOIN_API_KEY)  
-            .header("KC-API-SIGN", kc_sign)
-            .header("KC-API-TIMESTAMP", time_stamp.to_string())
-            .header("KC-API-PASSPHRASE", kc_pass_sign)
-            .header("KC-API-KEY-VERSION", kucoin_version)
-            .header("content-type", "application/json")
-            .body("")
-            .send()
-            .await?.text().await?;
-
-    let new_res = serde_json::from_str::<HashMap<String, Value>>(&res).unwrap();
+    let new_res = serde_json::from_str::<HashMap<String, Value>>(&res_str).unwrap();
 
     let acc_balance: &Vec<Value> = new_res.get("data").unwrap().as_array().unwrap();
 
-    for n in 0..=acc_balance.len() - 1 {
+    for n in 0..acc_balance.len() {
         //Object({"available": String("502.54391194"), "balance": String("502.54391194"), "currency": String("KDA"), "holds": String("0"), "id": String("6182514dc42972000142f847"), "type": String("trade")})
         let current_obj = &acc_balance[n];
         let sym : &str = &current_obj["currency"].as_str().unwrap();
         let balance : &f64 = &current_obj["balance"].as_str().unwrap().parse::<f64>().unwrap();
-        if *balance != 0.0 { println!("{} : {}", sym, balance)}
+
+        let price_endpoint : String = format!("{}?symbol={}-USDT", &kucoin_price_endpoint,sym);
+       
+        let price = get_kucoin_response(&price_endpoint, &current_user).await?;
+
+        let price_res = serde_json::from_str::<HashMap<String, Value>>(&price).unwrap();
+        
+        let ticker_price : f64 = price_res.get("data").unwrap()["price"].as_str().unwrap().parse::<f64>().unwrap();
+
+        if *balance != 0.0 { println!("{:.2} {} ({:.2} USD)", balance, sym, ticker_price * balance)}
+        //make the pool value next 
     }
 
-    //let mut file : File = File::create("./resFile")?;
-
-    //file.write_all(new_res.unwrap().get("data").unwrap().to_string().as_bytes());
-    
     Ok(())
 }
 
-
 /**
- * For the header of KC-API-SIGN: Use API-Secret to encrypt the prehash string {timestamp+method+endpoint+body} 
- * with sha256 HMAC. The request body is a JSON string and need to be the same with the parameters passed by the API. 
+ * Use API-Secret to encrypt the prehash string {timestamp+method+endpoint+body} with sha256 HMAC. 
+ * The request body is a JSON string and need to be the same with the parameters passed by the API. 
  * After that, use base64-encode to encrypt the result in step 1 again.
  * */
 fn sign(str_input : &str, secret_key : &str) -> String {
@@ -71,4 +67,34 @@ fn sign(str_input : &str, secret_key : &str) -> String {
     let temp_res = temp.result();
     let result_str = encode(temp_res.code());
     return result_str;
+}
+
+fn get_current_time_stamp() -> u128 {
+    return SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+}
+
+async fn get_kucoin_response(kucoin_endpoint : &str, current_user : &KucoinCred<'_>) -> Result<String, reqwest::Error> {
+    
+    let uri = format!("{}{}",current_user.kucoin_base_uri, kucoin_endpoint);
+
+    let client = reqwest::Client::new();
+ 
+    //constructing the timestamp 
+    let time_stamp : u128 = get_current_time_stamp();
+
+    let sign_str : &str = &format!("{}{}{}{}", &time_stamp, "GET", kucoin_endpoint, "");
+    let kc_sign : String = sign(sign_str, &current_user.kucoin_secret);
+
+    let kc_pass_sign : String = sign(&current_user.kucoin_passphrase, &current_user.kucoin_secret);
+    let kucoin_version : &str = "2";
+
+    let res = client.get(uri)
+        .header("KC-API-KEY", current_user.kucoin_api_key)  
+        .header("KC-API-SIGN", kc_sign)
+        .header("KC-API-TIMESTAMP", time_stamp.to_string())
+        .header("KC-API-PASSPHRASE", kc_pass_sign)
+        .header("KC-API-KEY-VERSION", kucoin_version)
+        .header("content-type", "application/json")
+        .body("").send().await?.text().await?;
+    return Ok(res);
 }
